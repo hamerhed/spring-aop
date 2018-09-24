9b7b3f3fbe67c4c7b87089ab5d7b696e92ca8d9e
Arkadiusz Radziuk <hamerhed@man.poznan.pl>
2018-09-24 22:08:56 +0200

    code refactored

---
+++ src/test/java/pl/hamerhed/aspects/counter/CounterAspectTest.java
@@ -0,0 +1,50 @@
+package pl.hamerhed.aspects.counter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import pl.hamerhed.aspects.counter.ArgumentsModifierAspect;
+import pl.hamerhed.domain.media.MediaPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"classpath:pl/hamerhed/aspects/counter/counter_aspects_config.xml"})
+public class CounterAspectTest {
+
+	@Autowired
+	private MediaPlayer warEternalPlayer;
+	
+	@Autowired
+	private MediaPlayer khaosLegionsPlayer;
+	
+	@Autowired
+	private ArgumentsModifierAspect counter;
+	
+	
+	@Test
+	public void testCountingAspect() throws Exception {
+		assertNotNull(warEternalPlayer);
+		assertNotNull(khaosLegionsPlayer);
+		assertNotNull(counter);
+		
+		warEternalPlayer.playAll();
+		khaosLegionsPlayer.playAll();
+		
+		assertEquals(2, counter.getPlayAllCounter());
+		
+		warEternalPlayer.playAll();
+		assertEquals(3, counter.getPlayAllCounter());
+		
+		warEternalPlayer.play(0);
+		warEternalPlayer.play(0);
+		khaosLegionsPlayer.play(0);
+		khaosLegionsPlayer.play(1);
+		
+		assertEquals(10, counter.getPlayTrackCounter());
+		
+	}
+}
